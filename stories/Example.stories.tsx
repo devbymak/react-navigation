8e696a10f7f1ac195bf5427ae94836b8ea46a48f
MohammadAli Karimi <ma.karimi76@gmail.com>
2021-09-21 03:20:41 +0430

    chore(stories): add example demo with Navigation

---
+++ stories/Example.stories.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Meta, Story } from '@storybook/react'
+import Navigation from '../src'
+
+const meta: Meta = {
+  title: 'Example',
+  component: Navigation,
+  argTypes: {
+    children: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
+  parameters: {
+    controls: { expanded: true },
+  },
+}
+
+export default meta
+
+const Template: Story = () => <Navigation>Demo</Navigation>
+
+export const Demo = Template.bind({})
+
+Demo.args = {}
